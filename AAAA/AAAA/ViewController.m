bbc6a7f13266f97ac0050b6830aa6a27d62ef7cb
Sam <790833597@qq.com>
2015-10-30 11:20:43 +0800

    版本1
    创建新项目

---
+++ AAAA/AAAA/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AAAA
+//
+//  Created by soft on 15/10/30.
+//  Copyright © 2015年 soft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
